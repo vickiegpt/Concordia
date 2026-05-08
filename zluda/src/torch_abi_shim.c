e899bb6b76c4f18f56e2944a11d89a80d932bfb7
Yiwei Yang <victoryang00@ucsc.edu>
2026-05-08 03:39:00 +0000

    add

--- zluda/src/torch_abi_shim.c
+++ zluda/src/torch_abi_shim.c
@@ -8,50 +8,7 @@ typedef void *cudaGraphConditionalHandle;
 typedef void *cudaStream_t;
 typedef int cudaStreamCaptureMode;
 
-cudaError_t cudaGraphAddNode_v2(cudaGraphNode_t *pGraphNode,
-                                cudaGraph_t graph,
-                                const cudaGraphNode_t *dependencies,
-                                size_t numDependencies,
-                                const void *nodeParams) {
-    (void)graph;
-    (void)dependencies;
-    (void)numDependencies;
-    (void)nodeParams;
-    if (pGraphNode) {
-        *pGraphNode = NULL;
-    }
-    return 0;
-}
-
-cudaError_t cudaGraphConditionalHandleCreate(cudaGraphConditionalHandle *pHandle,
-                                             cudaGraph_t graph,
-                                             unsigned int defaultLaunchValue,
-                                             unsigned int flags) {
-    (void)graph;
-    (void)defaultLaunchValue;
-    (void)flags;
-    if (pHandle) {
-        *pHandle = (cudaGraphConditionalHandle)(uintptr_t)0x1;
-    }
-    return 0;
-}
-
-cudaError_t cudaStreamBeginCaptureToGraph(cudaStream_t stream,
-                                          cudaGraph_t graph,
-                                          const cudaGraphNode_t *dependencies,
-                                          const void *dependencyData,
-                                          size_t numDependencies,
-                                          cudaStreamCaptureMode mode) {
-    (void)stream;
-    (void)graph;
-    (void)dependencies;
-    (void)dependencyData;
-    (void)numDependencies;
-    (void)mode;
-    return 0;
-}
-
-void hetgpu_torch_cuda_blas_gemm_half()
+void hetgpu_torch_cuda_blas_gemm_half() 
     __asm__("_ZN2at4cuda4blas4gemmIN3c104HalfES4_TnPNSt9enable_ifIXntaaoosr3std7is_sameIT_S4_EE5valuesr3std7is_sameIS6_NS3_8BFloat16EEE5valuesr3std7is_sameIT0_fEE5valueES6_E4typeELPS4_0EEEvcclllNS_10OpMathTypeIS6_E4typeEPKS6_lSH_lSF_PS8_l");
 void hetgpu_torch_cuda_blas_gemm_half() {}
 
